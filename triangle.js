a5b2f0b8870d45847637b89dc37b3a772621bf23
Melvin Gitau <gitaumel@gmail.com>
2018-08-14 15:47:51 +0300

    Add changes that have been added to triangle-tracker-project

---
+++ triangle.js
@@ -0,0 +1,19 @@
+function getTriangle(){
+
+var a = parseFloat(prompt("side a"));
+var b = parseFloat(prompt("side b"));
+var c = parseFloat(prompt("side c"));
+ if(a===b && a===c && b===c){
+alert("Equilateral");
+}
+else if(a===b || a===c || b===c){
+alert("Isosceles");
+}
+else if((a+b)<= c || (a+c)<=b || (b+c)<=a) {
+alert("Not a Triangle");
+}
+else{
+alert("Scalar");
+}
+}
+getTriangle();
